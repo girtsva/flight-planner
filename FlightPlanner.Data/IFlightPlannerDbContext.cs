8d3d29fdd71501b6fcfd799c398a8a65a0a7097f
Girts Varna <gvarna@gmail.com>
2022-03-21 14:34:33 +0200

    fix

--- FlightPlanner.Data/IFlightPlannerDbContext.cs
+++ FlightPlanner.Data/IFlightPlannerDbContext.cs
@@ -9,8 +9,8 @@ namespace FlightPlanner.Data
     {
         DbSet<T> Set<T>() where T : class;
         EntityEntry<T> Entry<T>(T entity) where T : class;
-        public DbSet<Flight> Flights { get; set; }
-        public DbSet<Airport> Airports { get; set; }
+        DbSet<Flight> Flights { get; set; }
+        DbSet<Airport> Airports { get; set; }
         int SaveChanges();
         Task<int> SaveChangesAsync();
     }
